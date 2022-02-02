6828a09f7933da52f1d81cc24d7fcf92491b92d5
MrSolarius <45536184+mrsolarius@users.noreply.github.com>
2022-02-02 18:02:21 +0100

    Feat : Encapsulation de la classe Cage pour respecter les contrainte se trouvant sur lesCages

--- src/java/fr/litopia/modele/Cage.java
+++ src/java/fr/litopia/modele/Cage.java
@@ -3,61 +3,68 @@ package fr.litopia.modele;
 import java.util.Set;
 
 public class Cage {
-  private Integer noCage;
-  private String fonction;
-  private long noAlle;
-  private Set<Animal> lstAnimals;
-  private Set<Gardien> lstGardiens;
-  
-  public Cage() {
-  }
+    private Integer noCage;
+    private String fonction;
+    private Integer noAlle;
+    private Set<Animal> lstAnimals;
+    private Set<Gardien> lstGardiens;
 
-  public Cage(Integer c) {
-    noCage = c;
-  }
+    public Cage() {}
 
-  public Integer getNoCage() {
-    return noCage;
-  }
+    public Cage(Integer c) {
+        setNoCage(c);
+    }
 
-  public void setNoCage(Integer c) {
-    noCage = c;
-  }
-  
-  public String getFonction() {
-    return fonction;
-  }  
+    public Integer getNoCage() {
+        return noCage;
+    }
 
-  public void setFonction(String f) {
-    fonction = f;
-  }
+    public void setNoCage(Integer c) throws IllegalArgumentException {
+        // Restriction de la cage comme impose par la BD dan la constraint LesCages_C2
+        if (c < 1 || c > 999) {
+            throw new IllegalArgumentException("noAlle must be between 1 and 999");
+        }
+        noCage = c;
+    }
 
-  public long getNoAlle() {
-    return noAlle;
-  }
- 
-  public void setNoAlle(long n) {
-    noAlle = n;
-  }
-  
-  public Set<Animal> getAnimals() {
-	    return lstAnimals;
-	  }
-	  
-  public void setAnimals(Set<Animal> l) {
-	    lstAnimals = l;
-	  }
+    public String getFonction() {
+        return fonction;
+    }
 
-  public Set<Gardien> getGardiens() {
-	    return lstGardiens;
-	  }
-	  
-public void setGardiens(Set<Gardien> l) {
-	    lstGardiens = l;
-	  }
+    public void setFonction(String f) {
+        fonction = f;
+    }
 
-      @Override
-      public String toString() {
+    public Integer getNoAlle() {
+        return noAlle;
+    }
+
+    public void setNoAlle(Integer n) throws IllegalArgumentException {
+        // Restriction de la cage comme impose par la BD dans la constraint LesCages_C3
+        if (n < 1 || n > 999) {
+            throw new IllegalArgumentException("noAlle must be between 1 and 999");
+        }
+        noAlle = n;
+    }
+
+    public Set<Animal> getAnimals() {
+        return lstAnimals;
+    }
+
+    public void setAnimals(Set<Animal> l) {
+        lstAnimals = l;
+    }
+
+    public Set<Gardien> getGardiens() {
+        return lstGardiens;
+    }
+
+    public void setGardiens(Set<Gardien> l) {
+        lstGardiens = l;
+    }
+
+    @Override
+    public String toString() {
         return "Cage [noCage=" + noCage + ", fonction=" + fonction + ", noAlle=" + noAlle + "]";
-      }
+    }
 }
